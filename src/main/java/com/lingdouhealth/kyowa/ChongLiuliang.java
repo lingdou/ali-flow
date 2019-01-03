8f749cc80dc175a05e32da27074f9e59b1ed901f
吴鲁华 <wuluhua2017@gmail.com>
2019-01-03 11:48:37 +0800

    POST 请求改成 GET 请求，添加充值结果的返回值

--- src/main/java/com/lingdouhealth/kyowa/ChongLiuliang.java
+++ src/main/java/com/lingdouhealth/kyowa/ChongLiuliang.java
@@ -10,15 +10,54 @@ import com.aliyuncs.profile.IClientProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by billy on 2018/12/28.
  */
 @Controller
-@RequestMapping("flow")
+@RequestMapping("flow2")
 public class ChongLiuliang {
+    /**
+     * 发送流量
+     * @param phone 手机号
+     * @param offerId 订单Id
+     * @throws ClientException
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    private String sendFlow(String phone, Long offerId, String outOrderId, String aliKey, String aliSecret) throws ClientException {
+        client = getClient(aliKey, aliSecret);
+        logger.info("充值请求 手机号 {} offerId {} outOrderId {} aliKey {} aliSecret", phone, offerId, outOrderId, aliKey, aliSecret);
+        // TODO out订单号需要外部传参数
+        CreateCdpOrderRequest ltReq = new CreateCdpOrderRequest();
+        ltReq.setPhoneNumber(phone);
+        ltReq.setOutOrderId(outOrderId);
+        ltReq.setOfferId(offerId);
+        ltReq.setReason("测试流量赠送");
+        System.out.println("电信OutOrderId " + outOrderId);
+        CreateCdpOrderResponse ltResponse = client.getAcsResponse(ltReq);
+        if ("OK".equals(ltResponse.getCode())) {
+            //业务处理
+            CreateCdpOrderResponse.Data data = ltResponse.getData();
+            System.out.println("ResultCode:" + data.getResultCode());
+            System.out.println("ResultMsg:" + data.getResultMsg());
+            System.out.println("OrderId:" + data.getOrderId());
+            System.out.println("ExtendParam:" + data.getExtendParam());
+            return "true";
+        } else {
+            // 说明请求超时，没有拿到结果，需要用该outOrderId重试，获取幂等结果
+            //如果重试获取的CreateCdpOrderResponse.Code为10004或00000则可标识下单成功
+            //10004表示重复的outOrderId，说明可能是上次超时的请求已下单成功，或之前你用过该outOrderId下过单
+            //你自己要保证该outOrderId的唯一
+            //00000表示下单成功，之前超时的请求没有成功。
+            return "false";
+        }
+    }
+
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private IAcsClient client;
@@ -35,40 +74,5 @@ public class ChongLiuliang {
         DefaultProfile.addEndpoint("cn‐hangzhou", "cn‐hangzhou", product, domain);
         client = new DefaultAcsClient(profile);
         return client;
-    }
-
-    /**
-     * 发送流量
-     * @param phone 手机号
-     * @param offerId 订单Id
-     * @throws ClientException
-     */
-    @RequestMapping(produces = {"application/json; charset=utf-8"})
-    @ResponseBody
-    private void sendFlow(String phone, Long offerId, String outOrderId, String aliKey, String aliSecret) throws ClientException {
-        client = getClient(aliKey, aliSecret);
-        logger.info("充值请求 手机号 {} offerId {}", phone, offerId);
-        // TODO out订单号需要外部传参数
-        CreateCdpOrderRequest ltReq = new CreateCdpOrderRequest();
-        ltReq.setPhoneNumber(phone);
-        ltReq.setOutOrderId(outOrderId);
-        ltReq.setOfferId(offerId);
-        ltReq.setReason("测试流量赠送");
-        System.out.println("电信OutOrderId " + outOrderId);
-        CreateCdpOrderResponse ltResponse = client.getAcsResponse(ltReq);
-        if ("OK".equals(ltResponse.getCode())) {
-            //业务处理
-            CreateCdpOrderResponse.Data data = ltResponse.getData();
-            System.out.println("ResultCode:" + data.getResultCode());
-            System.out.println("ResultMsg:" + data.getResultMsg());
-            System.out.println("OrderId:" + data.getOrderId());
-            System.out.println("ExtendParam:" + data.getExtendParam());
-        } else {
-            // 说明请求超时，没有拿到结果，需要用该outOrderId重试，获取幂等结果
-            //如果重试获取的CreateCdpOrderResponse.Code为10004或00000则可标识下单成功
-            //10004表示重复的outOrderId，说明可能是上次超时的请求已下单成功，或之前你用过该outOrderId下过单
-            //你自己要保证该outOrderId的唯一
-            //00000表示下单成功，之前超时的请求没有成功。
-        }
     }
 }
